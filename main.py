157c8240b8a4704befbf59a362a54c5878ffbba4
Scott Serven <scottserven@gmail.com>
2022-04-17 21:35:25 -0400

    initial source

---
+++ main.py
@@ -0,0 +1,29 @@
+import dotenv
+import discord
+from wordle.game import WordleGameHandler
+
+# List of registered game handlers, so this bot could handle more than one
+game_handlers = [
+    WordleGameHandler()
+]
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    # ignore messages from the bot itself
+    if message.author == client.user:
+        return
+
+    for game_handler in game_handlers:
+        if message.content.lower().startswith(game_handler.command_prefix.lower()):
+            await game_handler.process_message(message)
+
+
+config = dotenv.dotenv_values("env/.env")
+client.run(config['BOT_TOKEN'])
