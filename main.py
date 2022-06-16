3e85ed019dd14da24e60f54ad76146bc576a8d29
Esther <26940960+ezstarr@users.noreply.github.com>
2022-06-16 12:57:27 -0700

    trying to fix wordle

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import discord
-from dotenv import load_dotenv
+import dotenv
 import logging
 import os
 import storage.postgres
 from datetime import datetime
 import json
 # imports for twitch announcements:
-import requests
-from discord.ext import tasks, commands
-from discord.utils import get
+# import requests
+# from discord.ext import tasks, commands
+# from discord.utils import get
 
 
 # Opens .env file
-load_dotenv(".env")
+dotenv.load_dotenv(".env")
 logger = logging.getLogger(__name__)
 
 
@@ -22,24 +22,24 @@ logger = logging.getLogger(__name__)
 
 
 # twitch announcements:
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='/', intents=intents)
+# intents = discord.Intents.all()
+# bot = commands.Bot(command_prefix='/', intents=intents)
 
 
 # Authentication with Twitch API.
-client_id = os.environ['CLIENT_ID']
-client_secret = os.environ['CLIENT_SECRET']
-body = {
-    'client_id': client_id,
-    'client_secret': client_secret,
-    "grant_type": 'client_credentials'
-}
-r = requests.post('https://id.twitch.tv/oauth2/token', body)
-keys = r.json()
-headers = {
-    'Client-ID': client_id,
-    'Authorization': 'Bearer ' + keys['access_token']
-}
+# client_id = os.environ['CLIENT_ID']
+# client_secret = os.environ['CLIENT_SECRET']
+# body = {
+#     'client_id': client_id,
+#     'client_secret': client_secret,
+#     "grant_type": 'client_credentials'
+#     }
+# r = requests.post('https://id.twitch.tv/oauth2/token', body)
+# keys = r.json()
+# headers = {
+#     'Client-ID': client_id,
+#     'Authorization': 'Bearer ' + keys['access_token']
+#     }
 
 
 game_storage = None
@@ -145,22 +145,22 @@ def log_and_print_exception(e):
 
 
 # Command to add Twitch usernames to the json.
-@bot.command(name='timeenjoyed', help='Adds your Twitch to the live notifs.', pass_context=True)
-async def add_twitch(ctx, twitch_name):
-    # Opens and reads the json file.
-    with open('streamers.json', 'r') as file:
-        streamers = json.loads(file.read())
-
-    # Gets the users id that called the command.
-    user_id = ctx.author.id
-    # Assigns their given twitch_name to their discord id and adds it to the streamers.json.
-    streamers[user_id] = twitch_name
-
-    # Adds the changes we made to the json file.
-    with open('streamers.json', 'w') as file:
-        file.write(json.dumps(streamers))
-    # Tells the user it worked.
-    await ctx.send(f"Added {twitch_name} for {ctx.author} to the notifications list.")
+# @bot.command(name='timeenjoyed', help='Adds your Twitch to the live notifs.', pass_context=True)
+# async def add_twitch(ctx, twitch_name):
+#     # Opens and reads the json file.
+#     with open('streamers.json', 'r') as file:
+#         streamers = json.loads(file.read())
+#
+#     # Gets the users id that called the command.
+#     user_id = ctx.author.id
+#     # Assigns their given twitch_name to their discord id and adds it to the streamers.json.
+#     streamers[user_id] = twitch_name
+#
+#     # Adds the changes we made to the json file.
+#     with open('streamers.json', 'w') as file:
+#         file.write(json.dumps(streamers))
+#     # Tells the user it worked.
+#     await ctx.send(f"Added {twitch_name} for {ctx.author} to the notifications list.")
 
 
 def run_interaction_bot():
